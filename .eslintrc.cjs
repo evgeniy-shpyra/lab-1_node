ae5ab223575928e2964e6632986d0525d5642560
Evgeniy <foksmix256@gmail.com>
2022-10-16 11:24:24 +0300

    add sonarjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,14 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-  }
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ["standard", 'plugin:sonarjs/recommended'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ['sonarjs'],
+    rules: {},
 }
